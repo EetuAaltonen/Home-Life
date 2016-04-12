dc7837ba3f9515ab6171b48f10c8d0fa4243a57f
Eetu Aaltonen <eetu.aaltonen98@gmail.com>
2016-04-12 19:53:34 +0300

    First Git Push

---
+++ eetu_script/eetu_index.js
@@ -0,0 +1,32 @@
+$(function(){
+  $("#top_banner").load("eetu_includes/eetu_top_banner.html");
+  $("#button_area").load("eetu_includes/eetu_button_area.html");
+  $("#action_area").load("eetu_includes/eetu_action_area.html");
+});
+function etusivu_action_area() {
+ $("#action_area").load("eetu_includes/eetu_action_area.html");
+}
+function talous_action_area() {
+ $("#action_area").load("eetu_includes/eetu_talous_action_area.html");
+}
+function ruokalista_action_area() {
+  $("#action_area").load("eetu_includes/eetu_ruokalista_action_area.html");
+}
+function siivousvuorot_action_area() {
+  $("#action_area").load("eetu_includes/eetu_siivousvuorot_action_area.html");
+}
+function kauppalista_action_area() {
+  $("#action_area").load("eetu_includes/eetu_kauppalista_action_area.html");
+}
+function kalenteri_action_area() {
+  $("#action_area").load("eetu_includes/eetu_kalenteri_action_area.html");
+}
+function liikuntamittari_action_area() {
+  $("#action_area").load("eetu_includes/eetu_liikuntamittari_action_area.html");
+}
+function muistilistat_action_area() {
+  $("#action_area").load("eetu_includes/eetu_muistilistat_action_area.html");
+}
+function muutosseuranta_action_area() {
+  $("#action_area").load("eetu_includes/eetu_muutosseuranta_action_area.html");
+}
