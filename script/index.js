a51f96bc683ade127ba105b15a64293a3242b500
Eetu Aaltonen <eetu.aaltonen98@gmail.com>
2016-04-12 22:49:47 +0300

    fixing

--- script/index.js
+++ script/index.js
@@ -1,32 +1,32 @@
 $(function(){
-  $("#top_banner").load("eetu_includes/eetu_top_banner.html");
-  $("#button_area").load("eetu_includes/eetu_button_area.html");
-  $("#action_area").load("eetu_includes/eetu_action_area.html");
+  $("#top_banner").load("includes/top_banner.html");
+  $("#button_area").load("includes/button_area.html");
+  $("#action_area").load("includes/action_area.html");
 });
 function etusivu_action_area() {
- $("#action_area").load("eetu_includes/eetu_action_area.html");
+ $("#action_area").load("includes/action_area.html");
 }
 function talous_action_area() {
- $("#action_area").load("eetu_includes/eetu_talous_action_area.html");
+ $("#action_area").load("includes/talous_action_area.html");
 }
 function ruokalista_action_area() {
-  $("#action_area").load("eetu_includes/eetu_ruokalista_action_area.html");
+  $("#action_area").load("includes/ruokalista_action_area.html");
 }
 function siivousvuorot_action_area() {
-  $("#action_area").load("eetu_includes/eetu_siivousvuorot_action_area.html");
+  $("#action_area").load("includes/siivousvuorot_action_area.html");
 }
 function kauppalista_action_area() {
-  $("#action_area").load("eetu_includes/eetu_kauppalista_action_area.html");
+  $("#action_area").load("includes/kauppalista_action_area.html");
 }
 function kalenteri_action_area() {
-  $("#action_area").load("eetu_includes/eetu_kalenteri_action_area.html");
+  $("#action_area").load("includes/kalenteri_action_area.html");
 }
 function liikuntamittari_action_area() {
-  $("#action_area").load("eetu_includes/eetu_liikuntamittari_action_area.html");
+  $("#action_area").load("includes/liikuntamittari_action_area.html");
 }
 function muistilistat_action_area() {
-  $("#action_area").load("eetu_includes/eetu_muistilistat_action_area.html");
+  $("#action_area").load("includes/muistilistat_action_area.html");
 }
 function muutosseuranta_action_area() {
-  $("#action_area").load("eetu_includes/eetu_muutosseuranta_action_area.html");
+  $("#action_area").load("includes/muutosseuranta_action_area.html");
 }
